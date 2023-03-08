a53c41ffdb869bcc4eb8942738e62ced68db3343
erpesh <89254828+erpesh@users.noreply.github.com>
2023-03-08 16:11:52 +0000

    changed header

---
+++ search.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Documents | Document Management</title>
+  <link rel="stylesheet" href="css/desktop.css"/>
+</head>
+
+<body>
+  <?php
+  // include("includes/header.php");
+  ?>
+  
+  <div class="page-container">
+    Search page
+  </div>
+
+  <?php
+  // include("includes/footer.php");
+  ?>
+
+  <script src="js/main.js"></script>
+</body>
+
+</html>
